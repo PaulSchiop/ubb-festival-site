5cd21c219b7efa3529b7cf87bc2ce421eef27296
PaulSchiop <schioppaul@gmail.com>
2026-03-29 18:29:34 +0300

    fix(contact): added google maps hyperlink to adress

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -18,21 +18,21 @@ export default function Contact() {
             <p className={styles.description}>Ai o întrebare sau vrei să fii partener? Scrie-ne pe email și echipa noastră îți va răspunde.</p>
 
             <div className={styles.contactDetails}>
-               
+
               <div className={styles.detailItem}>
                 <strong>Email:</strong>
                 <a href={`mailto:${config.contact.email}`}>{config.contact.email}</a>
               </div>
-              
+
               <div className={styles.detailItem}>
                 <strong>Telefon:</strong>
-                <span>Ilca Andreea - Coordonator Principal</span>
+                <a>Ilca Andreea - Coordonator Principal</a>
                 <a href={`tel:${config.contact.phone}`}>{config.contact.phone}</a>
               </div>
-              
+
               <div className={styles.detailItem}>
-                <strong>Adresă:</strong> 
-                <span>{config.contact.address}</span>
+                <strong>Adresă:</strong>
+                <a href="https://www.google.com/maps/place//data=!4m2!3m1!1s0x47490e809171d439:0xbf81fdd421ec007b?sa=X&ved=1t:8290&ictx=111" target="_blank" rel="noopener noreferrer">{config.contact.address}</a>
               </div>
 
             </div>
